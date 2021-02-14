39102e8cb458f6f4229b918c38a8e251794578c2
Electromasta <electromasta@gmail.com>
2021-02-14 16:03:33 -0500

    Setup-Angular
    Initial checkin of angular and angular materials

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+
+const routes: Routes = [
+  {path: '', component: HeroComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
